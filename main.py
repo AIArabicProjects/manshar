c0d74c1420ff0304fb6e20d780c60d15c10a0867
Mohanad Kaleia <ms.kaleia@gmail.com>
2025-05-25 23:44:20 +0300

    add logger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import lib.rss
 import lib.summarizer
+import lib.logger
 from clients.facebook.client import Client as FacebookClient
 from clients.x.client import Client as XClient
 from clients.linkedin.client import Client as LinkedinClient
@@ -10,6 +11,8 @@ from config import x as x_config
 from config import linkedin as linkedin_config
 from config import telegram as telegram_config
 
+logger = lib.logger.get_logger(__name__)
+
 def read_history(filename="history.txt"):
     try:
         with open(filename, "r") as f:
@@ -17,46 +20,37 @@ def read_history(filename="history.txt"):
     except FileNotFoundError:
         return set()
 
-if __name__ == "__main__":  
-    # TODO:
-    # 1. Fetch the latest article
-    # 2. check if it was published before in the history file
-    # 3. term if it was published already
-    # 4. otherwise, summarize the article
-    # 5. publish on x
-    # 6. publish on facebook
-    # 7. publish on linkedin
-    # 8. publish on telegram
-    # 9. update the history file
-     
+if __name__ == "__main__":               
     article = lib.rss.fetch_latest_article()    
     
     # Read the history.txt and check if the id in the file or not
     history = read_history()
     if article["id"] in history:
-        print(f"Already posted: {article['id']}")
+        logger.info(f"already posted: {article['id']}")
         exit(0)
         
     message = f"{article['title']}\n\n{article['link']}"    
     
     fb_client = FacebookClient(facebook_config)
     response = fb_client.send(message)    
+    logger.info(f"facebook response: {response}")
 
     x_client = XClient(x_config)
-    response = x_client.send(message)    
+    response = x_client.send(message)
+    logger.info(f"X response: {response}")
 
     # TODO: LinkedIn client still needs testing as the api key is not generated yet
     # linkedin_client = LinkedinClient(linkedin_config)
     # response = linkedin_client.send("Hello World! Hello AI in Arabic!", link="https://example.com")    
+    # logger.info(f"linkedin response: {response}")
 
     # Send to Telegram
     telegram_client = TelegramClient(telegram_config)
     response = telegram_client.send(message)
+    logger.info(f"telegram response: {response}")
 
     # append the id to the history file
     with open("history.txt", "a") as f:
         f.write(article["id"] + "\n")
     
-    
-    
-
+    logger.info(f"published article {article['id']} successfully")
